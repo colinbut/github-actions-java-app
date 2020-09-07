eb13654b634b1f598608de91344368d493c130fb
Colin But <colin.but@outlook.com>
2020-09-07 21:54:20 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/githubactionsjavaapp/GithubActionsJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.githubactionsjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/githubactionsjavaapp/GithubActionsJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.githubactionsjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
